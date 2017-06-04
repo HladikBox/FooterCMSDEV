38d61b57e249e065a66021d3e0eb6677c813872d
Steve <alucard263096@126.com>
2017-06-05 01:30:42 +0800

    价值插件配置菜单

--- include/init.inc.php
+++ include/init.inc.php
@@ -51,7 +51,30 @@ for($i=0;$i<count($MenuArray["mainmenus"]["mainmenu"]);$i++){
     }
 }
 
-if($_SESSION[SESSIONNAME]["modellist"]==null){
+$path=USER_ROOT."plugin.xml";
+$fp = fopen($path,"r");
+$str = fread($fp,filesize($path));
+$PluginArray=xmlToArray($str);
+$menucount=count($MenuArray["mainmenus"]["mainmenu"]);
+$pluginConfigMenu=array();
+
+foreach ($PluginArray as $key => $value) {
+	if($value["cmsmenu"]!=""){
+		$cmsmenu=explode(",", $value["cmsmenu"]);
+		foreach ($cmsmenu as $ck => $cv) {
+			$MenuArray["mainmenus"]["mainmenu"][$menucount]["module"]="plugin_config";
+			$MenuArray["mainmenus"]["mainmenu"][$menucount]["name"]=$SysLang["model"]["systempluginconfig"];
+			$MenuArray["mainmenus"]["mainmenu"][$menucount]["onlyadmin"]=1;
+			$subcount=count($MenuArray["mainmenus"]["mainmenu"][$menucount]["submenus"]["submenu"]);
+			$MenuArray["mainmenus"]["mainmenu"][$menucount]["submenus"]["submenu"][$subcount]["model"]=$cv;
+			$MenuArray["mainmenus"]["mainmenu"][$menucount]["submenus"]["submenu"][$subcount]["onlyadmin"]=1;
+		}
+	}
+}
+
+
+
+if(1==1||$_SESSION[SESSIONNAME]["modellist"]==null){
 	
 	for($i=0;$i<count($MenuArray["mainmenus"]["mainmenu"]);$i++){
 	    for($j=0;$j<count($MenuArray["mainmenus"]["mainmenu"][$i]["submenus"]["submenu"]);$j++){
