33201fc232a57b65f759d7e4797e6003a6e81c4d
Steve Cai <steve.cai@morningstar.com>
2017-07-24 16:34:34 +0800

    COMMIT

--- classes/mgr/common_function.php
+++ classes/mgr/common_function.php
@@ -211,4 +211,18 @@ function request_get($url) {
     return $desfilename;
     
   }
+  
+  function saveBase64ToImage($imageData,$module)
+	$dir = USER_ROOT."/upload/$module/";
+	$fileName = date("YmdHis").floor(microtime() * 1000).'.jpg';
+	try
+	{
+	$imageData = str_replace(" ","+",$imageData);
+	$binImageData = base64_decode($imageData);
+	$FP = fopen($dir.$fileName,"w"); 
+	fwrite($FP,$binImageData);
+	return $fileName;
+  }
+  
+  
 ?>
