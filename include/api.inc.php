676a7fcc2e4a969759de03bc3f598bb5ec5ce997
Steve.cai <alucard263096@126.com>
2017-12-01 22:47:10 +0800

    fix for ajax with session

--- include/api.inc.php
+++ include/api.inc.php
@@ -1,10 +1,10 @@
 <?php
 
  if(MODULE=="api"){
-    header('Access-Control-Allow-Origin:*');  
-    header('Access-Control-Allow-Methods:POST');  
+    header('Access-Control-Allow-Credentials:true');  
+    header('Access-Control-Allow-Origin:'.$_SERVER['HTTP_ORIGIN']);  
+    header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE');  
     header('Access-Control-Allow-Headers:x-requested-with,content-type,TokenKey,Sign,Fmd5str,lang');  
-
     if(strtolower($_SERVER["REQUEST_METHOD"])=="options"){
 		exit();
 	}
