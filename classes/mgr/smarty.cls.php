fe57e3bdcea594fc376e6b48f9841e9ce8043cbd
Steve <alucard263096@126.com>
2017-12-29 02:37:22 +0800

    commit for new style

--- classes/mgr/smarty.cls.php
+++ classes/mgr/smarty.cls.php
@@ -28,7 +28,10 @@ $smarty->right_delimiter="}}";
  $smarty->assign('uploadpath',USER_PATH.$CONFIG['fileupload']['upload_path']);
  $smarty->assign('request_url_encode',base64_encode($_SERVER["REQUEST_URI"]));
  $smarty->assign('parenturl',base64_decode($_REQUEST["parenturl"]));
- $smarty->assign('frontendurl',$CONFIG['frontendurl']);
  $smarty->assign('support_multilang',$CONFIG["SupportMultiLanguage"]?"1":"0");
+ if($CONFIG["CmsStyle"]==""){
+ 	$CONFIG["CmsStyle"]="AdminLTE";
+ }
+ $smarty->assign('CmsStyle',$CONFIG["CmsStyle"]);
 
 ?>
